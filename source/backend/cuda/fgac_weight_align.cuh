53a4e7fc18a0c826abfef0db6621e7d77a1becb4
lvchengTSH <45536776+lvcheng1229@users.noreply.github.com>
2024-03-12 01:17:40 +0800

    cuda

--- source/backend/cuda/fgac_weight_align.cuh
+++ source/backend/cuda/fgac_weight_align.cuh
@@ -55,12 +55,56 @@ __device__ void compute_lowest_and_highest_weight(
 	float* cut_high_weight_error
 )
 {
+	float rcp_stepsize = 1.0;
 	for (unsigned int sp = 0; sp < max_angular_steps; sp++)
 	{
+		float minidx(128.0f);
+		float maxidx(-128.0f);
+		float errval = 0.0;
+		float cut_low_weight_err = 0.0;
+		float cut_high_weight_err = 0.0;
+		float offset = offsets[sp];
+		
 		for (unsigned int j = 0; j < weight_count; j++)
 		{
+			float sval = dec_weight_ideal_value[j] * rcp_stepsize - offset;
+			float svalrte = round(sval);
+			float diff = sval - svalrte;
+			errval += diff * diff;
 
+			// Reset tracker on min hit
+			minidx = minidx < svalrte ? svalrte : minidx;
+			cut_low_weight_err = svalrte < minidx ? 0.0 : cut_low_weight_err;
+
+			// Accumulate on min hit
+			float accum = cut_low_weight_err + float(1.0f) - float(2.0f) * diff;
+			cut_low_weight_err = (svalrte == minidx) ? accum : cut_low_weight_err;
+
+			// Reset tracker on max hit
+			maxidx = svalrte > maxidx ? svalrte : maxidx;
+			cut_high_weight_err = svalrte > maxidx ? 0.0 : cut_high_weight_err;
+
+			// Accumulate on max hit
+			accum = cut_high_weight_err + float(1.0f) + float(2.0f) * diff;
+			cut_high_weight_err = (svalrte == maxidx) ? accum : cut_high_weight_err;
 		}
+
+		// Write out min weight and weight span; clamp span to a usable range
+		int span = int(maxidx - minidx + 1.0f);
+		span = std::min(span, int(max_quant_steps + 3));
+		span = std::max(span, 2);
+		lowest_weight[sp] = minidx;
+		weight_span[sp] = span;
+
+		// The cut_(lowest/highest)_weight_error indicate the error that results from  forcing
+		// samples that should have had the weight value one step (up/down).
+		float ssize = 1.0f / rcp_stepsize;
+		float errscale = ssize * ssize;
+		error[sp] = errval * errscale;
+		cut_low_weight_error[sp] = cut_low_weight_err * errscale;
+		cut_high_weight_error[sp] = cut_high_weight_err * errscale;
+
+		rcp_stepsize = rcp_stepsize + 1.0;
 	}
 }
 
@@ -84,6 +128,12 @@ __device__ void compute_angular_endpoints_for_quant_levels(
 	float error[ANGULAR_STEPS];
 	float cut_low_weight_error[ANGULAR_STEPS];
 	float cut_high_weight_error[ANGULAR_STEPS];
+	compute_lowest_and_highest_weight(weight_count, dec_weight_ideal_value,
+		max_angular_steps, max_quant_steps,
+		angular_offsets, lowest_weight, weight_span, error,
+		cut_low_weight_error, cut_high_weight_error);
+
+
 }
 
 
